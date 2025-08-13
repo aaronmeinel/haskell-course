1aabbb6b62369ffcbc9020847cb56ed0e5fba7ef
Aaron Meinel <aaron.meinel@procura-inventory.com>
2025-08-13 08:50:56 +0200

    feat: mesocycle

--- src/Mesocycle.hs
+++ src/Mesocycle.hs
@@ -1,3 +1,54 @@
 module Mesocycle where
 
--- Data types and logic for Mesocycle will go here
+import WorkoutTemplate
+
+-- Enum types for feedback
+
+data Soreness = NeverSore | HealedAWhileAgo | HealedJustInTime | StillSore
+  deriving (Show, Eq, Enum)
+
+data JointPain = NoPain | LowPain | ModeratePain | HighPain
+  deriving (Show, Eq, Enum)
+
+data Pump = LowPump | ModeratePump | AmazingPump
+  deriving (Show, Eq, Enum)
+
+data Workload = Easy | PrettyGood | PushedLimits | TooMuch
+  deriving (Show, Eq, Enum)
+
+-- Updated feedback types
+
+data PreExerciseFeedback = PreExerciseFeedback
+  { soreness   :: Maybe Soreness
+  } deriving (Show, Eq)
+
+data PostExerciseFeedback = PostExerciseFeedback
+  { jointPain  :: Maybe JointPain
+  , pump       :: Maybe Pump
+  , workload   :: Maybe Workload
+  } deriving (Show, Eq)
+
+data Mesocycle = Mesocycle
+  { template      :: WorkoutTemplate
+  , numWeeks      :: Int
+  , weeks         :: [MesocycleWeek]
+  } deriving (Show, Eq)
+
+data MesocycleWeek = MesocycleWeek
+  { weekNumber    :: Int
+  , workouts      :: [MesocycleWorkout]
+  } deriving (Show, Eq)
+
+data MesocycleWorkout = MesocycleWorkout
+  { workoutName   :: String
+  , exercises     :: [MesocycleExercise]
+  } deriving (Show, Eq)
+
+data MesocycleExercise = MesocycleExercise
+  { exerciseName     :: String
+  , muscleGroup      :: MuscleGroup
+  , prescribedSets   :: Int
+  , prescribedReps   :: Maybe Int
+  , preFeedback      :: Maybe PreExerciseFeedback
+  , postFeedback     :: Maybe PostExerciseFeedback
+  } deriving (Show, Eq)
--- test/Main.hs
+++ test/Main.hs
@@ -3,8 +3,8 @@ module Main where
 import Test.Hspec
 import Data.Yaml (decodeFileEither, ParseException)
 import Data.Either (isRight)
-import WorkoutTemplate (WorkoutTemplate)
-
+import WorkoutTemplate (WorkoutTemplate, MuscleGroup(..))
+import Mesocycle (Mesocycle(..), MesocycleWeek(..), MesocycleWorkout(..), MesocycleExercise(..), PreExerciseFeedback(..), PostExerciseFeedback(..), Soreness(..), JointPain(..), Pump(..), Workload(..))
 
 
 main :: IO ()
@@ -14,4 +14,21 @@ main = hspec $ do
             result <- decodeFileEither "workout_template.yaml" :: IO (Either Data.Yaml.ParseException WorkoutTemplate)
             result `shouldSatisfy` isRight
             -- You can add more detailed checks once FromJSON is implemented
+     describe "Mesocycle domain model" $ do
+        it "constructs a Mesocycle with correct structure" $ do
+          let meso = Mesocycle
+                { template = undefined  -- You can use a real template if available
+                , numWeeks = 4
+                , weeks = [ MesocycleWeek 1 [ MesocycleWorkout "Push" [ MesocycleExercise "Bench Press" Chest 4 (Just 10) (Just (PreExerciseFeedback (Just NeverSore))) (Just (PostExerciseFeedback (Just NoPain) (Just ModeratePump) (Just PrettyGood))) ] ] ]
+                }
+          numWeeks meso `shouldBe` 4
+          weekNumber (head (weeks meso)) `shouldBe` 1
+          let firstExercise = case weeks meso of
+                (MesocycleWeek _ (MesocycleWorkout _ (MesocycleExercise name _ _ _ _ _ : _) : _) : _) -> name
+                _ -> ""
+          firstExercise `shouldBe` "Bench Press"
+          let firstSoreness = case weeks meso of
+                (MesocycleWeek _ (MesocycleWorkout _ (MesocycleExercise _ _ _ _ (Just (PreExerciseFeedback (Just s))) _ : _) : _) : _) -> Just s
+                _ -> Nothing
+          firstSoreness `shouldBe` Just NeverSore
 
